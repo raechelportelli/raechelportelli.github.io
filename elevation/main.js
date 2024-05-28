ff273a8f6ecad20b977fd84e2486871362863e63
Raechel Portelli <88565111+raechelportelli@users.noreply.github.com>
2024-05-28 11:05:31 -0400

    Update main.js

--- elevation/main.js
+++ elevation/main.js
@@ -1,4 +1,4 @@
 var map = L.map('map').setView([42.44836, -83.97135], 12);
 
-var tiles = L.tileLayer.mbTiles('tiles_test.mbtiles').addTo(map);
+var tiles = L.tileLayer.mbTiles('tiles_test.mbtiles').addTo(map)
 
