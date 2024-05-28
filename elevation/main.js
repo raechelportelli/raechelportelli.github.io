bc9619b9c9c27096572ba3b7d52a2941bff53091
Raechel Portelli <88565111+raechelportelli@users.noreply.github.com>
2024-05-28 11:16:44 -0400

    Update main.js

--- elevation/main.js
+++ elevation/main.js
@@ -1,11 +1,11 @@
 var map = L.map('map').setView([42.44836, -83.97135], 12);
 
-var MBTS = L.tileLayer.mbTiles('tiles_test.mbtiles').addTo(map)
+var MBTS = L.tileLayer.mbTiles('tiles_test.mbtiles').addTo(map);
 
-map.createPane('imagePane')
-map.getPane(imagePane').style.zIndex=20
+map.createPane('imagePane');
+map.getPane('imagePane').style.zIndex = 20;
 
-var OSMMap = L.tileLayer.mbTiles('tiles20.mbtiles'.{
-                                 pane" 'imagePane'
-}).addTo(map)
+var OSMMap = L.tileLayer.mbTiles('tiles20.mbtiles', {
+    pane: 'imagePane'
+}).addTo(map);
 
